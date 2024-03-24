7086652f9935d7bfcee25ff3433e51cc240213da
lucasmilhoranca <lucas1234milhoranca@gmail.com>
2024-03-24 11:44:10 -0300

    feat: finalizando exercicio 7

---
+++ exercicios/7-manipulacao-e-controle-de-objetos/script.js
@@ -0,0 +1,85 @@
+class Tv {
+    constructor() {
+        this.ligada = false
+        this.marca
+    }
+
+    ligar() {
+        this.ligada = true
+
+        document.getElementById("status-tv").innerHTML = `
+            <span>TV Ligada</span>
+        `
+        const tvScreen = document.getElementById("status-tv");
+        tvScreen.textContent = "Ligada";
+        tvScreen.classList.add("tv-screen-on");
+    }
+
+    desligar() {
+        this.ligada = false
+
+        document.getElementById("status-tv").innerHTML = `
+            <span>TV Desligada</span>
+        `
+        const tvScreen = document.getElementById("status-tv");
+        tvScreen.textContent = "Desligada";
+        tvScreen.classList.remove("tv-screen-on");
+    }
+}
+
+class Controle {
+    constructor() {
+        this.marca
+    }
+
+    ligarTv(tv) {
+        this.marca === tv.marca ? tv.ligar() : document.getElementById("result").innerHTML = `<span>Controle não é compatível com a TV</span>`
+    }
+
+    desligarTv(tv) {
+        this.marca === tv.marca ? tv.desligar() : document.getElementById("result").innerHTML = `<span>Controle não é compatível com a TV</span>`
+    }
+}
+
+const meuControle = new Controle()
+const minhaTv = new Tv()
+
+function ligar() {
+    const marcaControle = document.getElementById("controle").value
+    const marcaTV = document.getElementById("tv").value
+
+    const result = document.getElementById("result")
+
+    result.innerHTML = ''
+
+    meuControle.marca = marcaControle
+    minhaTv.marca = marcaTV
+
+    if (!minhaTv.ligada) {
+        meuControle.ligarTv(minhaTv)
+    } else {
+        result.innerHTML = `
+            <span>Desligue a TV antes de liga-la novamente</span>
+        `
+    }
+}
+
+function desligar() {
+    const marcaControle = document.getElementById("controle").value
+    const marcaTV = document.getElementById("tv").value
+
+    const result = document.getElementById("result")
+
+    result.innerHTML = ''
+
+    meuControle.marca = marcaControle
+    minhaTv.marca = marcaTV
+
+    if (minhaTv.ligada) {
+        meuControle.desligarTv(minhaTv)
+    } else {
+        result.innerHTML = `
+            <span>Ligue a TV antes de desliga-la novamente</span>
+        `
+    }
+}
